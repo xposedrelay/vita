1f03e5fad4f484ad3390698b371998bd76732d2a
xposedrelay <martinnehuen2005@hotmail.com>
2025-04-08 19:37:24 -0300

    experimental: fix: chmod the script
    experimental: fix: change place of snippet

--- files/scripts/papirus-folders.sh
+++ files/scripts/papirus-folders.sh
@@ -4,6 +4,8 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+chmod +x /usr/bin/papirus-folders
+
 /usr/bin/papirus-folders -C green --theme Papirus-Dark
 /usr/bin/papirus-folders -C green --theme Papirus
 /usr/bin/papirus-folders -C green --theme ePapirus
